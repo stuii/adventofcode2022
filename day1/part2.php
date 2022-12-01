a5df2a1a2ae3e45c62a89e4766458c21acf9e39e
stui <me@stui.ch>
2022-12-01 13:09:15 +0100

    imporvements

--- day1/part2.php
+++ day1/part2.php
@@ -6,15 +6,12 @@ $elves = [0];
 $index = 0;
 foreach ($list as $cal) {
     if ($cal === '') {
-        $index++;
-        $elves[$index] = 0;
+        $elves[++$index] = 0;
     } else {
         $elves[$index] += (int)$cal;
     }
 }
 
-rsort($elves);
-
-$elvesSum = array_sum(array_slice($elves, 0, 3));
+$elvesSum = array_sum(array_slice(rsort($elves), 0, 3));
 
 Solver::setResult($elvesSum);
